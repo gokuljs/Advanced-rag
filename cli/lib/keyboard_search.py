962fca4560a4afd4f217bf93c3ac06924c80260a
Gokul Js <gokuljs029@gmail.com>
2026-02-10 07:18:14 +0530

    Enhance keyword search functionality by adding text transformation and tokenization. Implement matching token logic to improve search accuracy in movie titles.

--- cli/lib/keyboard_search.py
+++ cli/lib/keyboard_search.py
@@ -1,13 +1,30 @@
-from cli.lib.search_utils import load_movies
+import string
+from lib.search_utils import load_movies
 
 def transform_text(str):
-    return str.lower()
+    text = str.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    return text
+
+def tokenize_text(text):
+    text = transform_text(text)
+    token = [tok for tok in text.split() if tok]
+    return token
+
+def has_matching_tokens(query_tokens, movie_tokens):
+    for query_tok in query_tokens:
+        for movie_tok in movie_tokens:
+            if query_tok in movie_tok:
+                return True
+    return False
 
 def search_command(query, n_results):
     movies = load_movies()
     res = []
+    query_tokens = tokenize_text(query)
     for movie in movies:
-        if query.lower() in movie["title"].lower():
+        movie_tokens = tokenize_text(movie["title"])
+        if has_matching_tokens(query_tokens, movie_tokens):
             res.append(movie)
         if len(res) == n_results:
             break
